2c771a774a0a1c27177268a062dddaed0b09e99e
bishal0922 <bishal0922@gmail.com>
2024-03-01 11:45:46 -0600

    working code to display + add opencv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, Response, url_for
+import cv2
 
 app = Flask(__name__)
 
@@ -14,13 +15,29 @@ status_data = {
 
 @app.route("/")
 def index():
-    with open("index.html") as f:
-        return f.read()
+    video_feed_url = url_for('video_feed')  # Get the URL for the video feed
+    return render_template('index.html', video_feed_url=video_feed_url)
 
 @app.route("/api/status", methods=["GET"])
 def status():
     # Return the status data as JSON
     return jsonify(status_data)
 
+def gen_frames():
+    camera = cv2.VideoCapture(2)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(debug=True)
